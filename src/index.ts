f1c9d3ba65b5c4fee9b2cce8eab910b463b96a9a
Blake Sweet <blake.sweet16@gmail.com>
2020-09-17 04:00:28 -0400

    Add, Remove and List functionality

--- src/index.ts
+++ src/index.ts
@@ -40,10 +40,9 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 		let joinedChannel = newState.channel;
 		let discordServer = newState.guild;
 		
-		// See if voiceStateUpdate is for a listed channel pair
 		if(joinedChannel) { // User Joins a voice channel
 			// Add the user to the text chat permissions
-			let foundChannelPairIndex = channelPairList.findIndex(channelPair => channelPair.voiceChannel.channelID === newState.channelID); // Compare with the Voice Channel ID from the user
+			let foundChannelPairIndex = channelPairList.findIndex(channelPair => channelPair.voiceChannel.id === newState.channelID); // Compare with the Voice Channel ID from the user
 			if (foundChannelPairIndex >= 0){ 
 				// Define permissions
 				let overwriteOptions = { 
@@ -52,17 +51,17 @@ client.on('voiceStateUpdate', (oldState, newState) => {
 
 				// Find channel and update it
 				let discordTextChannel = discordServer.channels.cache.find(discordChannel => {
-					return discordChannel.id === channelPairList[foundChannelPairIndex].textChannel.channelID;
+					return discordChannel.id === channelPairList[foundChannelPairIndex].textChannel.id;
 				});
 				discordTextChannel!.updateOverwrite(newState.member!, overwriteOptions);
 			}
 		} else if (!joinedChannel && oldState.channel) { // User leaves a voice channel
 			// Remove the user from the text chat permissions
-			let foundChannelPairIndex = channelPairList.findIndex(channelPair => channelPair.voiceChannel.channelID === oldState.channelID); // Compare with the Voice Channel ID from the user
+			let foundChannelPairIndex = channelPairList.findIndex(channelPair => channelPair.voiceChannel.id === oldState.channelID); // Compare with the Voice Channel ID from the user
 			if (foundChannelPairIndex >= 0){ 
 				// Find channel and update it
 				let discordTextChannel = discordServer.channels.cache.find(discordChannel => {
-					return discordChannel.id === channelPairList[foundChannelPairIndex].textChannel.channelID;
+					return discordChannel.id === channelPairList[foundChannelPairIndex].textChannel.id;
 				});
 				discordTextChannel!.permissionOverwrites.get(newState.member!.id)!.delete(); // Remove user from channel overwrites. 
 			}
@@ -76,7 +75,7 @@ client.on('message', message =>{
 	 * @param title - Title of the message embed
 	 * @param content - Message content. This should contain the majority of the message
 	 */
-	function displayMessage (title: string, content: string){
+	function displayMessage (title: string = '', content: string = ''){
 		let newMessage = {
 			"embed": {
 				"color": 160860,
@@ -94,6 +93,7 @@ client.on('message', message =>{
 	
 	let messageContentUpper = message.content.toUpperCase(); 	// Used to provide non-case sensitive commands
 	let messageArrayUpper = messageContentUpper.split(' '); 	// Used to access command information (non-case sensitive)
+	var messageArray = message.content.split(' '); 				// Used to access command information
 	
 	// List all commands
 	if(messageContentUpper === `${prefix}` || messageContentUpper === `${prefix}`[0]){ 
@@ -106,6 +106,112 @@ client.on('message', message =>{
 		// Do nothing for DMs
 	}
 
-	// else if(messageArrayUpper[0] == `${prefix}` || messageArrayUpper[0] == `${prefix}`[0]){
-	// }
+	// Commands in format (PREFIX [Command] [... Parameters])
+	else if(messageArrayUpper[0] === `${prefix}` || messageArrayUpper[0] === `${prefix}`[0]){
+
+		// Create a channel pair
+		if (messageArrayUpper[1] == "ADD"){ 
+			if (messageArray.length !== 4){
+				displayMessage('Among add [Voice Channel ID] [Text Channel ID]', 'Create a new channel pair');
+			}
+			else{
+				if (+messageArrayUpper[2]){ // Verify Voice Channel ID is a number
+					if (+messageArrayUpper[3]){ // Verify Text Channel ID is a number
+						// Find channels for pair
+						let discordVoiceChannel = message.guild?.channels.cache.find(discordChannel => {
+							return discordChannel.id === messageArrayUpper[2];
+						});
+						let discordTextChannel = message.guild?.channels.cache.find(discordChannel => {
+							return discordChannel.id === messageArrayUpper[3];
+						});
+
+						// Build a new channel pair with the found channel information
+						let newVoiceChannel = new channel.ChannelObject(discordVoiceChannel?.id, discordVoiceChannel?.name, discordVoiceChannel?.type);
+						let newTextChannel = new channel.ChannelObject(discordTextChannel?.id, discordTextChannel?.name, discordVoiceChannel?.type);
+						channelPairList.push(new channel.ChannelPair(newVoiceChannel, newTextChannel));
+						displayMessage(`Channels Successfully paired`, `Voice Channel: **${discordVoiceChannel?.name}** \nText Channel: **${discordTextChannel?.name}**`);
+					} else{
+						displayMessage('Error', '__[Voice Channel ID]__ should be a number.');
+					}
+				} else {
+					displayMessage('Error', '__[Text Channel ID]__ should be a number.');
+				}
+			}
+		}
+
+		// Remove a channel pair
+		else if (messageArrayUpper[1] == "REMOVE"){ 
+			if (messageArray.length !== 4){
+				displayMessage('Among remove [Voice Channel ID] [Text Channel ID]', 'Remove a pre-existing channel pair.');
+			}
+			else{
+				if (+messageArrayUpper[2]){ // Verify Voice Channel ID is a number
+					if (+messageArrayUpper[3]){ // Verify Text Channel ID is a number
+						let foundVoiceChannel: channel.ChannelObject | undefined;
+						let foundTextChannel: channel.ChannelObject | undefined;
+						
+						// Remove the found channel pair
+						channelPairList.find((channelPair, index) => {
+							// See if either of the channels are found in each channel pair 
+							let voiceChannelMatches = (channelPair.voiceChannel.id === messageArrayUpper[2]);
+							let textChannelMatches = (channelPair.textChannel.id === messageArrayUpper[3]);
+							if (voiceChannelMatches){
+								foundVoiceChannel = channelPair.voiceChannel;
+							}
+							if (textChannelMatches){
+								foundTextChannel = channelPair.textChannel;
+							}
+
+							// Remove channel if found
+							if (voiceChannelMatches && textChannelMatches){
+								channelPairList.splice(index, 1); 
+							}
+						});
+
+						// Display status message 
+						if (foundVoiceChannel && foundTextChannel){
+							displayMessage('Channel pair removed', `Voice Channel: **${foundVoiceChannel.name}** \nText Channel: **${foundTextChannel?.name}**`);
+						} else if (!foundVoiceChannel){
+							displayMessage('Error', 'Unable to find a channel pair with the given __[Voice Channel]__ ID.');
+						} else if (!foundTextChannel){
+							displayMessage('Error', 'Unable to find a channel pair with the given __[Text Channel]__ ID.');
+						} else{
+							displayMessage('Error', 'Unable to find a channel pair with either channel IDs.');
+						}
+					} else{
+						displayMessage('Error', '__[Voice Channel ID]__ should be a number.');
+					}
+				} else {
+					displayMessage('Error', '__[Text Channel ID]__ should be a number.');
+				}
+			}
+		}
+
+		// Display all current Channel Pairs
+		else if (messageArrayUpper[1] == "LIST"){ 
+			if (messageArray.length !== 2){
+				displayMessage('Among list', 'Display the current list of Channel Pairs.');
+			}
+			else{
+				// Get Channel Pair data
+				let pairString = '';
+				channelPairList.forEach((channelPair, index) => {
+					pairString += index !== 0 ? '\n\n' : ''; // Add line spacing
+					pairString += `${index+1}: \nVoice Channel: **${channelPair.voiceChannel.name}** \nText Channel: **${channelPair.textChannel.name}**`;
+				});
+				if (!pairString){
+					pairString = 'None';
+				}
+
+				// Display Channel Pairs
+				displayMessage('Channel Pairs', pairString);
+			}
+		}
+
+		// Unknown command
+		else{
+			displayMessage('Unknown Command', 'Try command \"**Among**\" to list available commands.');
+		} 
+		message.delete(); // Clean chat history
+	}
 })
